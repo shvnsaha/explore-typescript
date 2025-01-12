b3deb9e4c3b86b13d0f25a683dadd7c4a985f84a
shvnsaha <shovan.css2018@gmail.com>
2025-01-12 22:11:31 +0600

    Destructuring ts

---
+++ src/Destructuring.ts
@@ -0,0 +1,21 @@
+{
+    // Destructuring:
+
+    const user = {
+        id: 123,
+        name: {
+            firstName: "Shovan",
+            lastName: "Saha"
+        },
+        address: "Tangail",
+        contactNo: "123456"
+    }
+
+    const {contactNo:phoneNo ,name:{firstName} } = user;
+    console.log(phoneNo);
+
+
+    // array destructuring:
+    const myFriends: string[] = ['a','b','c','d','e'];
+    const [,,c,...rest] = myFriends
+}
