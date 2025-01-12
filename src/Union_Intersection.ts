ad506491c160f50f01fe57ace8e5652949948c1e
shvnsaha <shovan.css2018@gmail.com>
2025-01-12 22:27:49 +0600

    Union Intersection ts

---
+++ src/Union_Intersection.ts
@@ -0,0 +1,34 @@
+{
+    // Union Type:
+    type TFrontEndDev = 'GoodDev' | 'BadDev';
+    type TFullStack = 'Developer' | TFrontEndDev;
+    type TDeveloper = TFrontEndDev | TFullStack;
+    const newDeveloper:Developer = 'Developer';
+
+    type User = {
+        name: string;
+        age: number;
+        gender: 'Male' | 'Female'
+    }
+
+
+    // Intersection Type:
+
+    type FrontEnd = {
+        skills: string[];
+        role1: 'frontend dev'
+    }
+
+    type BackEnd = {
+        skills: string[];
+        role2: 'backend dev'
+    }
+
+    type Dev = FrontEnd & BackEnd;
+
+    const dev: Dev = {
+        skills:['HTML',"JS"],
+        role1:"frontend dev",
+        role2: "backend dev"
+    }
+}
