94bac2931a0d89932d29663488e64ea4e3aba450
shvnsaha <shovan.css2018@gmail.com>
2025-01-12 20:35:29 +0600

    Data type in ts

---
+++ src/DT.ts
@@ -0,0 +1,28 @@
+{
+    // Primitive Data Type:
+
+    let name: string = "Shovan Saha";
+    let age: number = 24;
+    let isAdmin: boolean = true;
+    let x:undefined = undefined;
+    let y:null = null;
+
+
+    let d: number;
+    // d = "String";
+    d = 123;
+    // d = true;
+
+
+    // Non Primitive Data Type:
+    // Array:
+
+    const friends: string[] = ['Rahul',"Karim"];
+    // friends.push('Saha',10)
+    const eligbleRollList: number[] = [1,2];
+   
+
+    // Tuple:
+    const NameAge: [string,number] = ['Shovan',24]
+
+}
