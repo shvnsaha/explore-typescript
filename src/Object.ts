8479cacdbe40f4e74216f88387bc7dc4b78364a2
shvnsaha <shovan.css2018@gmail.com>
2025-01-12 20:43:21 +0600

    Object type

---
+++ src/Object.ts
@@ -0,0 +1,17 @@
+{
+    // Object type:
+
+    const user: {
+        firstName: string;
+        middleName?: string;  // optional type
+        lastName: string;
+        age: number;
+        company: "CSS Company"  //literal type
+    } = {
+        firstName: "Shovan",
+        // middleName: "Hriday",
+        lastName: "Saha",
+        age: 24,
+        company: "CSS Company"
+    }
+}
